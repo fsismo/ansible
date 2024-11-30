53bb7fbd22fd74432d3283ebe3f916f381139a0a
sismo <fernando@sismonda.com.ar>
2024-11-30 04:54:27 -0300

    Add storage mount verification

--- ubu2404/phwrgrd/opt/utils/pihole-sync-slave.sh
+++ ubu2404/phwrgrd/opt/utils/pihole-sync-slave.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+NFS_MOUNT_POINT="/mnt/storage"  # Replace with your actual NFS mount point
+
+if ! mountpoint -q $NFS_MOUNT_POINT; then
+    echo "NFS is not mounted. Attempting to mount..."
+    sudo mount $NFS_MOUNT_POINT
+    if ! mountpoint -q $NFS_MOUNT_POINT; then
+        echo "Failed to mount NFS. Exiting."
+        exit 1
+    fi
+fi
+
 CHGPH=$(rsync -az -i --files-from=/opt/utils/pihole-sync-slave.conf /mnt/storage/pihole /opt/docker/phwrgrd/pihole/etc/pihole |grep 'f')
 
 if [ -n "$CHGDM" ] || [ -n "$CHGPH" ]; then
