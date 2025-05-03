8cee8a90089610d2ceb3d4b46498f8aabefdacd8
sismo <fernando@sismonda.com.ar>
2025-05-03 13:20:26 -0300

    Working configuration with ext-www

---
+++ ubu2404/ext-www/opt/docker/ext-www/sql/wordpress.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE `wp-sismonda`;
+CREATE USER 'wp-sismonda'@'%' IDENTIFIED BY 'yj9TxsLXqkw1xrj0dRaYY80gg1';
+GRANT ALL PRIVILEGES ON `wp-sismonda`.* TO 'wp-sismonda'@'%';
+FLUSH PRIVILEGES;
